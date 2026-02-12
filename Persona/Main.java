947f108255f86d2aa9cb0c909eb91f71f9f1c4ae
BrandonJuarez <juzabrandon@hotmail.com>
2026-02-12 09:10:38 -0600

    add Persona

---
+++ Persona/Main.java
@@ -0,0 +1,40 @@
+
+
+/**
+ * Class Main
+ */
+public class Main {
+
+  //
+  // Fields
+  //
+
+  
+ 
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  //
+  // Other methods
+  //
+
+  /**
+   * @param        args
+   */
+  public static void main(String[] args)
+  {
+  Persona myPerson = new Persona("Brandon", 23);
+  System.out.println("Hello");
+  myPerson.saludar();
+  System.out.println(myPerson.getEdad());
+  }
+
+
+}
